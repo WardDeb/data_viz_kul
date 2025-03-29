4196590be3c2d955a275b7b56870fed1e788b1ba
wardDeb <w@rddeboutte.com>
2025-03-29 10:50:02 +0100

    it probably helps to RTFM

--- marimo/deployment.py
+++ marimo/deployment.py
@@ -7,24 +7,46 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-    import matplotlib.pyplot as plt
-    import plotly.express as px
+    return (mo,)
+
+
+@app.cell
+def _():
+    import micropip
+    return (micropip,)
+
+
+@app.cell
+async def _(micropip):
+    await micropip.install("plotly")
+    await micropip.install("pandas")
+    import plotly
+    return (plotly,)
+
+
+@app.cell
+def _():
     import pandas as pd
-    import polars as pl
-    import pyarrow
-    return mo, pd, pl, plt, px, pyarrow
+    import plotly.express as px
+    return pd, px
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""# Pig feeding interactive example""")
+    mo.md(
+        r"""
+        # Pig feeding interactive example
+
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        """
+    )
     return
 
 
 @app.cell
 def _(mo, pd):
     df = pd.read_csv(
-        str(mo.notebook_location() / 'processed_data/subset.csv')
+        str(mo.notebook_location() / 'public/subset.csv')
     )
     # Redundant information can be removed
     del df['date']
@@ -32,12 +54,6 @@ def _(mo, pd):
     df['start'] = pd.to_datetime(df['start'])
     df['end'] = pd.to_datetime(df['end'])
     return (df,)
-
-
-@app.cell
-def _(mo):
-    mo.md(r"""# Example of an interactive plot""")
-    return
 
 
 @app.cell
